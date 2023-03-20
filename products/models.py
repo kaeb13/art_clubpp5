3b36d52e9fab24e29dec4b4d128ec1bb21cd0143
kaeb13 <kaskebba@gmail.com>
2023-03-20 17:29:13 +0000

    Edit json file to Django and add model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name
+
+
+class Poster(models.Model):
+    FORMAT_CHOICES = (
+        ('vertical', 'Vertical'),
+        ('horizontal', 'Horizontal'),
+    )
+
+    title = models.CharField(max_length=80)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    format_type = models.CharField(max_length=15, choices=FORMAT_CHOICES)
+    image_path = models.CharField(max_length=155)
+    is_new_arrival = models.BooleanField()
+
+    def __str__(self):
+        return self.title
